9be57be4fb01eda17a50058f737c5476d22a3c1a
Nff0928 <nicolas.fonseca0928@gmail.com>
2025-10-17 14:06:07 -0500

    se agregan planes y se mejora CEO

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-  },
-})
+export default defineConfig({ plugins: [react(), tailwindcss()], })
